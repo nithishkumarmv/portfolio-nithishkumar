c96440f84223eb3174252d19fce6cd74fd741836
NITHISHKUMAR <nithishmessi75400@gmail.com>
2024-06-23 01:51:28 +0530

    updated v1.0

--- js/script.js
+++ js/script.js
@@ -23,3 +23,29 @@ document.addEventListener('DOMContentLoaded', function() {
     addHoverAnimation(skillBars);
     addHoverAnimation(interestBars);
 });
+
+
+function downloadPDF() {
+    const link = document.createElement('a');
+    link.href = 'resume/nithish_resume.pdf'; // Replace with the actual path to your PDF file
+    link.download = 'nithish_resume.pdf'; // Replace with the desired file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function sendEmail() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var location = document.getElementById("location").value;
+    var message = document.getElementById("message").value;
+
+    var mailtoLink = "mailto:nithishkumarmv10@gmail.com"
+        + "?subject=" + encodeURIComponent("Contact Form Submission from " + name)
+        + "&body=" + encodeURIComponent("Name: " + name + "\n"
+        + "Email: " + email + "\n"
+        + "Location: " + location + "\n\n"
+        + "Message:\n" + message);
+
+    window.location.href = mailtoLink;
+}
